df508b885e432200155dd1d0e72ad11694c96ef3
jordany <jordanyluiz@gmail.com>
2022-06-15 11:02:13 -0300

    removing the translation of data

--- controller/request.py
+++ controller/request.py
@@ -2,17 +2,15 @@ import json
 from isbnlib import info
 from isbnlib import meta
 from isbnlib.registry import bibformatters
-from google_trans_new import google_translator
 
 def request(isbn):
     try:
-        translator = google_translator()
         language = info(isbn) # Isso contém o país 
         bibtex = bibformatters["json"]
         metadata = json.loads(bibtex(meta(isbn)))
         metadata['language'] = language
-        metadata['type'] = translator.translate(metadata['type'], lang_tgt='pt')
-        metadata['language'] = translator.translate(metadata['language'], lang_tgt='pt')
+        metadata['type'] = metadata['type']
+        metadata['language'] = metadata['language']
         authors = list()
         for author in metadata['author']:
             authors.append(author['name'])
