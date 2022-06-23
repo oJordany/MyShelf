c8a02f300adec8f7386da59b4028f886ba8c686d
jordany <jordanyluiz@gmail.com>
2022-06-23 01:10:15 -0300

    modififying IWantToReadWindow

--- interface/screens/IWantToReadWindow.py
+++ interface/screens/IWantToReadWindow.py
@@ -20,11 +20,34 @@ class IWantToReadWindow:
         icon = PhotoImage(file=relative_to_assets("Logo.png"))
         self.IWTR_window.iconphoto(False, icon)
         self.IWTR_window.title("I Want to Read")
-        self.IWTR_window.geometry("460x460")
+        self.IWTR_window.geometry("460x445")
         self.IWTR_window.configure(bg = "#2C0A59")
+
+    def generate_gmail(self):
+        from random import randint
+        import requests
+        ID = randint(0,1000)
+        # access the API
+        url = "https://temp-gmail.p.rapidapi.com/get"
+        querystring = {"id":ID,"type":"estanteVirtual"}
+        headers = {
+            'x-rapidapi-host': "temp-gmail.p.rapidapi.com",
+            'x-rapidapi-key': "YOUR PRIVATE KEY"
+            }
+        
+        # send a request to the API
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        
+        # convert the response to JSON format 
+        json_response = response.json()
+        # get gmail address
+        gmail = json_response['items']['username']
+        # get gmail password
+        password = json_response['items']['key']
 
     def catch_date(self):
         self.date = self.calendar.get_date()
+        self.email = self.entry_email.get()
         self.IWTR_window.destroy()
 
     def generate_IWTR_window(self):
@@ -34,7 +57,7 @@ class IWantToReadWindow:
         canvas = Canvas(
             self.IWTR_window,
             bg = "#2C0A59",
-            height = 460,
+            height = 445,
             width = 460,
             bd = 0,
             highlightthickness = 0,
@@ -61,11 +84,11 @@ class IWantToReadWindow:
         self.calendar.config(background="#B22FD3", locale="en", date_pattern="yyyy-mm-dd")
         self.calendar.place(x=105, y=100)
 
-        self.IWTR_window.btn_select_date_inactive = PhotoImage(file=relative_to_assets("button_select_date_inactive.png"))
-        self.IWTR_window.btn_select_date_active = PhotoImage(file=relative_to_assets("button_select_date.png"))
+        self.IWTR_window.btn_confirm_inactive = PhotoImage(file=relative_to_assets("button_confirm.png"))
+        self.IWTR_window.btn_confirm_active = PhotoImage(file=relative_to_assets("button_confirm_active.png"))
 
         button_image_select_date = PhotoImage(
-            file=relative_to_assets("button_select_date_inactive.png")
+            file=relative_to_assets("button_confirm.png")
         )
         
         button_select_date = Button(
@@ -81,24 +104,32 @@ class IWantToReadWindow:
             cursor="hand2",
         )
 
-        button_select_date.bind("<Enter>", lambda e: button_select_date.config(image=self.IWTR_window.btn_select_date_active))
-        button_select_date.bind("<Leave>", lambda e: button_select_date.config(image=self.IWTR_window.btn_select_date_inactive))
+        button_select_date.bind("<Enter>", lambda e: button_select_date.config(image=self.IWTR_window.btn_confirm_active))
+        button_select_date.bind("<Leave>", lambda e: button_select_date.config(image=self.IWTR_window.btn_confirm_inactive))
 
         button_select_date.place(
-            x=125,
-            y=285,
+            x=127,
+            y=360,
         )
 
-        entry_image_search = PhotoImage(
+        image_image_ETRRN= PhotoImage(
+            file=relative_to_assets("ETRRN.png"))
+        canvas.create_image(
+            230.0,
+            282,
+            image=image_image_ETRRN
+        )
+
+        entry_image_email = PhotoImage(
             file=relative_to_assets("entry_search.png"))
         
         canvas.create_image(
             230,
-            419,
-            image=entry_image_search
+            320,
+            image=entry_image_email
         )
 
-        self.entry_search = Entry(
+        self.entry_email = Entry(
             self.IWTR_window,
             bd=0,
             bg="#93679A",
@@ -107,9 +138,9 @@ class IWantToReadWindow:
             font=('Georgia 20')
         )
 
-        self.entry_search.place(
+        self.entry_email.place(
             x=55,
-            y=400,
+            y=300.5,
             width=350.0,
             height=30.75,
         )
